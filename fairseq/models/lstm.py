fc5518c57944b98928de5f85784bcafa46bcbb62
oncebasun <basunjhm@sina.com>
2019-04-20 17:52:25 -0400

    test

--- fairseq/models/lstm.py
+++ fairseq/models/lstm.py
@@ -273,7 +273,7 @@ class LSTMEncoder(FairseqEncoder):
         """Maximum input length supported by the encoder."""
         return int(1e5)  # an arbitrary large number
 
-
+'''
 class AttentionLayer(nn.Module):
     def __init__(self, input_embed_dim, source_embed_dim, bias=False):
         super().__init__()
@@ -304,6 +304,38 @@ class AttentionLayer(nn.Module):
         x = (attn_scores.unsqueeze(2) * source_hids).sum(dim=0)
 
         #x = F.tanh(self.output_proj(torch.cat((x, input), dim=1)))
+        return x, attn_scores
+'''
+class AttentionLayer(nn.Module):
+    def __init__(self, input_embed_dim, source_embed_dim, output_embed_dim, bias=False):
+        super().__init__()
+
+        self.input_proj = Linear(input_embed_dim, source_embed_dim, bias=bias)
+        self.output_proj = Linear(input_embed_dim + source_embed_dim, output_embed_dim, bias=bias)
+
+    def forward(self, input, source_hids, encoder_padding_mask):
+        # input: bsz x input_embed_dim
+        # source_hids: srclen x bsz x output_embed_dim
+
+        # x: bsz x output_embed_dim
+        x = self.input_proj(input)
+
+        # compute attention
+        attn_scores = (source_hids * x.unsqueeze(0)).sum(dim=2)
+
+        # don't attend over padding
+        if encoder_padding_mask is not None:
+            attn_scores = attn_scores.float().masked_fill_(
+                encoder_padding_mask,
+                float('-inf')
+            ).type_as(attn_scores)  # FP16 support: cast to float and back
+
+        attn_scores = F.softmax(attn_scores, dim=0)  # srclen x bsz
+
+        # sum weighted sources
+        x = (attn_scores.unsqueeze(2) * source_hids).sum(dim=0)
+
+        x = F.tanh(self.output_proj(torch.cat((x, input), dim=1)))
         return x, attn_scores
 
 
@@ -359,8 +391,8 @@ class LSTMDecoder(FairseqIncrementalDecoder):
         # We always have sememe attention!!!!!
         self.sem_attention = AttentionLayer(hidden_size, sem_embed_dim, bias=False)
 
-        self.fc_after_attn = Linear(hidden_size + encoder_output_units + sem_embed_dim, hidden_size, bias=False)
-        #self.fc_after_attn = Linear(hidden_size + encoder_output_units, hidden_size, bias=False)
+        #self.fc_after_attn = Linear(hidden_size + encoder_output_units + sem_embed_dim, hidden_size, bias=False)
+        self.fc_after_attn = Linear(hidden_size + encoder_output_units, hidden_size, bias=False)
 
         if hidden_size != out_embed_dim:
             self.additional_fc = Linear(hidden_size, out_embed_dim)
@@ -440,8 +472,8 @@ class LSTMDecoder(FairseqIncrementalDecoder):
             # test
             sem_out, sem_attn_scores[:, j, :] = self.sem_attention(hidden, sem_embeds, sem_padding_mask)
 
-            out = F.tanh(self.fc_after_attn(torch.cat((hidden, out, sem_out), dim=1)))
-            #out = self.fc_after_attn(torch.cat((hidden, out), dim=1))
+            #out = F.tanh(self.fc_after_attn(torch.cat((hidden, out, sem_out), dim=1)))
+            out = F.tanh(self.fc_after_attn(torch.cat((hidden, out), dim=1)))
 
             # input feeding
             input_feed = out
